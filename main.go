74505eb6d6f8d8f02aab177c8b71194d029dea0d
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-03-18 08:06:48 -0300

    parent and child

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		parent()
+	case "child":
+		child()
+	default:
+		panic("not found command")
+	}
+}
+
+func parent() {
+	a := append([]string{"child"}, os.Args[2:]...)
+	cmd := exec.Command("/proc/self/exe", a...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("error: ", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("error: ", err)
+		os.Exit(1)
+	}
+}
